66ee2ada7721b88ad07e29961187bffe90ca7a70
annamatos <anna.matos@sptech.school>
2024-11-05 07:41:16 -0300

    Adicionando criação do estabelecimento na Aditum

--- src/sections/Auth/Register/RegisterForm.tsx
+++ src/sections/Auth/Register/RegisterForm.tsx
@@ -13,11 +13,13 @@ import ColorContainer from "../../../components/Containers/ColorContainer/ColorC
 import { colors as c } from "../../../styles/Colors";
 import { EstablishmentAdapter } from "../../../adapters/Establishment/Establishment";
 import { EstablishmentRequestDto } from "../../../utils/Establishment/establishment.types";
+import { AditumEstablishmentRequest } from "../../../utils/Establishment/establishment.types";
 import { useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
 import { EmployeeRequestDto } from "../../../utils/Users/Employee/employee.types";
 import { EmployeeAdapter } from "../../../adapters/User/Employee/Employee";
 import Text from "../../../components/Texts/Text/Text";
+// import { AditumAdapter } from "../../../adapters/Aditum/Aditum";
 
 interface RegisterFormProps {
   step: number;
@@ -25,7 +27,7 @@ interface RegisterFormProps {
   handleSubmit: () => void;
   acc: string;
 }
-
+// adicionar Aditum 
 const RegisterForm: React.FC<RegisterFormProps> = ({
   step,
   setStep,
@@ -100,10 +102,89 @@ const RegisterForm: React.FC<RegisterFormProps> = ({
 
   const handleFinish = async () => {
     try {
+      const aditumEstablishmentReq = {
+        cnpj: fields.cnpj, 
+        documentType: "Cnpj",
+        email: fields.employeeEmail, 
+        merchantCode: "", 
+        fantasyName: fields.nome, 
+        socialName: fields.nome, 
+        mcc: 6010, 
+        acquirerSettings: [
+          {
+            acquirerId: 999,
+            merchantIdOnAcquirer: "123123",
+            merchantTokenOnAcquirer: "123132",
+            isActive: true,
+            aditumProduct: "Ecommerce",
+            aditumProductId: 1,
+          },
+          {
+            acquirerId: 999,
+            merchantIdOnAcquirer: "1231231",
+            merchantTokenOnAcquirer: "123",
+            isActive: true,
+            aditumProduct: "Pos",
+            aditumProductId: 2,
+          },
+        ],
+        address: {
+          street: fields.logradouro, 
+          number: fields.numero, 
+          neighborhood: fields.logradouro, 
+          city: fields.city, 
+          state: fields.estado, 
+          country: "Brasil", 
+          zipCode: fields.cep.replace(/[^\d]/g, "")
+        },
+        contacts: [
+          {
+            name: "Joice", 
+            phone: {
+              countryCode: "55", 
+              areaCode: "11", 
+              number: "98123-3131", 
+              type: "Commercial", 
+            },
+            email: "joice.matos@gmail.com", 
+            type: "Administration", 
+          },
+        ],
+        type: 2, 
+        profile: {
+          smartCheckoutLimitAmount: 150000, 
+          antifraudMandatory: false, 
+          acceptForeignCustomer: false, 
+          disputeManagementEnabled: false, 
+          hideRefreshToken: true, 
+          antifraudCheckFirst: false, 
+          ignoreAcquirerSettingsOnUpdate: true, 
+          allowInheritTokenizationConfiguration: false, 
+          needsValidatePaymentLinkOnAcquirer: false, 
+          overridesChildsAntifraudSettings: false, 
+          encodeWebhookPayloadContent: true, 
+          verificationRequiredBeforeCancellation: false, 
+          useUuidOnMerchantChargeId: true, 
+          acquirerTransactionFilter: "Undefined", 
+          useParentAntifraudSettings: false, 
+          useParentMerchantPixSettings: false, 
+          disableSelfFinanceInSmartCheckout: false, 
+        },
+        bankSlipSettings: [], 
+        parentMerchantId: "a9296dbc-6372-4f8a-959d-40ff30135a2f", 
+        status: "Active", 
+        isActive: true, 
+        relatedMerchants: [
+          "a9296dbc-6372-4f8a-959d-40ff30135a2f", 
+          "ce869b77-a2b2-4b95-8863-2f74733a3929", 
+        ],
+        
+      } as AditumEstablishmentRequest;
+
       const establishmentFields = {
         name: fields.nome,
         imgUrl: fields.imgUrl,
-        aditumId: "aditumTeste",
+        aditumId: "aditumTeste", 
         local: {
           number: fields.numero,
           floor: 0,
@@ -125,10 +206,10 @@ const RegisterForm: React.FC<RegisterFormProps> = ({
 
       const establishmenteAdapter = new EstablishmentAdapter();
       const establishmentResponse =
-        await establishmenteAdapter.registerEstablishment(establishmentFields);
-
+      await establishmenteAdapter.registerEstablishment(establishmentFields, aditumEstablishmentReq);
+  
       console.log(establishmentResponse);
-
+  
       const employeeFields = {
         name: fields.employeeNome,
         email: fields.employeeEmail,
@@ -154,12 +235,12 @@ const RegisterForm: React.FC<RegisterFormProps> = ({
           number: fields.employeePhone.replace(/[^\d]/g, "").slice(4),
         },
       } as EmployeeRequestDto;
-
+  
       const employeeAdapter = new EmployeeAdapter();
       const employeeResponse = await employeeAdapter.create(employeeFields);
-
+  
       console.log(employeeFields);
-
+  
       if (employeeResponse && establishmentResponse) {
         navigate(`/establishment/${establishmentResponse.id}`);
       } else {
@@ -178,6 +259,7 @@ const RegisterForm: React.FC<RegisterFormProps> = ({
       });
     }
   };
+  
 
   return (
     <ColorContainer bgColor={c.gray100}>
